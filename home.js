78881d8abf2fedbb43fc19dfdfb02a323ca702d8
yuribrito01 <yuridebatista@gmail.com>
2022-04-28 16:40:10 -0300

    Reconstrução do site
    Neste commit, estou mudando algumas coisas no meu site, como por exemplo estou cirando botoes para acesso a minhas informaçoes

--- home.js
+++
@@ -1,8 +0,0 @@
-$('#exampleModal').modal('//#region ')
-
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
